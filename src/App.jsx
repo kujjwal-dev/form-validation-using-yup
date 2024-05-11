4c72be4e3f46c41f956bbaf7c735ac346ac56331
kujjwal-dev <trptrp098@gmail.com>
2024-05-11 17:04:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import FormWihoutYup from './Form-Wihout-Yup'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <FormWihoutYup/>
+    </>
+  )
+}
+
+export default App
